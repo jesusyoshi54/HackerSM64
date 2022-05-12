f0b8c6e8c8c9a6ebd7af614356fa29ef15031eed
scut <scut>
2022-05-12 13:02:05 -0400

    init finish of lvl

--- levels/castle_grounds/area_1/geo.inc.c
+++ levels/castle_grounds/area_1/geo.inc.c
@@ -3,7 +3,8 @@
 const GeoLayout castle_grounds_area_1_geo[] = {
 	GEO_NODE_START(),
 	GEO_OPEN_NODE(),
-		GEO_TRANSLATE_ROTATE_WITH_DL(LAYER_OPAQUE, 0, -576, 0, 0, 90, 0, castle_grounds_dl_Plane_mesh_layer_1),
+		GEO_TRANSLATE_NODE_WITH_DL(LAYER_OPAQUE, 904, -99, -1201, castle_grounds_dl_main_004_mesh_layer_1),
+		GEO_TRANSLATE_NODE_WITH_DL(LAYER_OPAQUE, 0, -432, 0, castle_grounds_dl_Plane_mesh_layer_1),
 	GEO_CLOSE_NODE(),
 	GEO_RETURN(),
 };
@@ -21,7 +22,7 @@ const GeoLayout castle_grounds_area_1[] = {
 		GEO_OPEN_NODE(),
 			GEO_CAMERA_FRUSTUM_WITH_FUNC(45.0000, 100, 30000, geo_camera_fov),
 			GEO_OPEN_NODE(),
-				GEO_CAMERA(CAMERA_MODE_8_DIRECTIONS, 0, 0, 0, 0, -100, 0, geo_camera_main),
+				GEO_CAMERA(CAMERA_MODE_8_DIRECTIONS, 0, -144, 0, 0, -244, 0, geo_camera_main),
 				GEO_OPEN_NODE(),
 					GEO_BRANCH(1, castle_grounds_area_1_geo),
 					GEO_RENDER_OBJ(),
